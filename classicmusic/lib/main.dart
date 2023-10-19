ea68d820fe0f9e841f863ea8ec3c61eb019ca89e
asmaembr <asmae.moubr@gmail.com>
2023-10-19 21:00:24 +0100

    changes

--- classicmusic/lib/main.dart
+++ classicmusic/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:just_audio_background/just_audio_background.dart';
 import 'package:rxdart/rxdart.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -43,6 +44,127 @@ class AudioPlayerScreen extends StatefulWidget {
 class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
   late AudioPlayer _audioPlayer;
 
+  final _playlist = ConcatenatingAudioSource(
+    children: [
+      AudioSource.uri(
+        Uri.parse('assets/audio/Arana.mp3'),
+        tag: MediaItem(
+          id: '0',
+          title: 'Arana',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/violon-marron-sur-textile-blanc-d9_2kPJBG0U'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/Big_Easy.mp3'),
+        tag: MediaItem(
+          id: '1',
+          title: 'Big Easy',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/chitarra-elettrica-verde-acqua-e-marrone-phS37wg8cQg'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/Blue_Monk.mp3'),
+        tag: MediaItem(
+          id: '2',
+          title: 'Blue Monk',
+          artist: 'Public Domain',
+          artUri: Uri.parse('https://unsplash.com/fr/photos/dBWvUqBoOU8'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/Coldness.mp3'),
+        tag: MediaItem(
+          id: '3',
+          title: 'Coldness',
+          artist: 'Public Domain',
+          artUri: Uri.parse('https://unsplash.com/fr/photos/Mx9vRuQpVkI'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/If_I_Wait.mp3'),
+        tag: MediaItem(
+          id: '4',
+          title: 'If I Wait',
+          artist: 'Public Domain',
+          artUri: Uri.parse('https://unsplash.com/fr/photos/F7W1QP62psQ'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/Incredible.mp3'),
+        tag: MediaItem(
+          id: '5',
+          title: 'Incredible',
+          artist: 'Public Domain',
+          artUri: Uri.parse('https://unsplash.com/fr/photos/Nf3ko-k2TPI'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/Mladost_Club.mp3'),
+        tag: MediaItem(
+          id: '6',
+          title: 'Mladost Club',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/un-ordinateur-de-bureau-assis-sur-scene-avec-des-lumieres-de-scene-et-un-public-04aMmb0Ijw0'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/Mount_Fuji.mp3'),
+        tag: MediaItem(
+          id: '7',
+          title: 'Mount Fuji',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/lotto-assortito-di-copertine-di-dischi-jazz-su-rack-v8M_wbLqcZU'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/nouvelle_vague.mp3'),
+        tag: MediaItem(
+          id: '8',
+          title: 'nouvelle vague',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/une-personne-nageant-au-dessus-dun-recif-corallien-colore-9E9NsEiUGxg'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/something.mp3'),
+        tag: MediaItem(
+          id: '9',
+          title: 'Something',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/homme-en-veste-noire-assis-sur-une-chaise-rouge-d0y0wzmaXkU'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/The_Harold_Rubin.mp3'),
+        tag: MediaItem(
+          id: '10',
+          title: 'The father The son and The Harold Rubin',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/silhouette-dun-homme-jouant-du-saxophone-au-coucher-du-soleil-iyJyiaR_vwc'),
+        ),
+      ),
+      AudioSource.uri(
+        Uri.parse('assets/audio/The_Years.mp3'),
+        tag: MediaItem(
+          id: '11',
+          title: 'The Years',
+          artist: 'Public Domain',
+          artUri: Uri.parse(
+              'https://unsplash.com/fr/photos/raccolta-galleria-YDf2T-Uyq7U'),
+        ),
+      ),
+    ],
+  );
+
   Stream<PositionData> get _positionDataStream =>
       Rx.combineLatest3<Duration, Duration, Duration?, PositionData>(
         _audioPlayer.positionStream,
@@ -57,7 +179,13 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
   @override
   void initState() {
     super.initState();
-    _audioPlayer = AudioPlayer()..setAsset('assets/audio/music.mp3');
+    _audioPlayer = AudioPlayer();
+    _init();
+  }
+
+  Future<void> _init() async {
+    await _audioPlayer.setLoopMode(LoopMode.all);
+    await _audioPlayer.setAudioSource(_playlist);
   }
 
   @override
@@ -97,19 +225,35 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            StreamBuilder<SequenceState?>(
+              stream: _audioPlayer.sequenceStateStream,
+              builder: (context, snapshot) {
+                final state = snapshot.data;
+                if (state?.sequence.isEmpty ?? true) {
+                  return const SizedBox();
+                }
+                final metadata = state!.currentSource!.tag as MediaItem;
+                return MediaMetadata(
+                  imageUrl: metadata.artUri.toString(),
+                  artist: metadata.artist ?? '',
+                  title: metadata.title,
+                );
+              },
+            ),
+            const SizedBox(height: 20),
             StreamBuilder<PositionData>(
               stream: _positionDataStream,
               builder: (context, snapshot) {
                 final positionData = snapshot.data;
                 return ProgressBar(
                   barHeight: 8,
-                  baseBarColor: Colors.grey[700], 
-                  bufferedBarColor:Colors.grey,
-                  progressBarColor: Colors.black, 
-                  thumbColor: Colors.black, 
+                  baseBarColor: Colors.grey[700],
+                  bufferedBarColor: Colors.grey,
+                  progressBarColor: Colors.black,
+                  thumbColor: Colors.black,
                   timeLabelTextStyle: const TextStyle(
                     color: Colors.black,
-                    fontWeight: FontWeight.w600, 
+                    fontWeight: FontWeight.w600,
                   ),
                   progress: positionData?.position ?? Duration.zero,
                   buffered: positionData?.bufferedPosition ?? Duration.zero,
@@ -127,6 +271,66 @@ class _AudioPlayerScreenState extends State<AudioPlayerScreen> {
   }
 }
 
+class MediaMetadata extends StatelessWidget {
+  const MediaMetadata({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.artist,
+  });
+  final String imageUrl;
+  final String title;
+  final String artist;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        DecoratedBox(
+          decoration: BoxDecoration(
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(2, 4),
+                blurRadius: 4,
+              ),
+            ],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: CachedNetworkImage(
+              imageUrl: imageUrl,
+              height: 300,
+              width: 300,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          artist,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+          textAlign: TextAlign.center,
+        )
+      ],
+    );
+  }
+}
+
 class Controls extends StatelessWidget {
   const Controls({
     super.key,
@@ -137,7 +341,15 @@ class Controls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<PlayerState>(
+    return Row(mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      IconButton(
+        onPressed: audioPlayer.seekToPrevious, 
+        iconSize: 60,
+        color: Colors.black, 
+        icon: const Icon(Icons.skip_previous_rounded),
+        ),
+    StreamBuilder<PlayerState>(
       stream: audioPlayer.playerStateStream,
       builder: (context, snapshot) {
         final playerState = snapshot.data;
@@ -164,6 +376,14 @@ class Controls extends StatelessWidget {
           size: 80,
         );
       },
+    ),
+    IconButton(
+      onPressed: audioPlayer.seekToNext,
+      iconSize: 60, 
+      color: Colors.black, 
+      icon: const Icon(Icons.skip_next_rounded),
+      ),
+    ],
     );
   }
 }
